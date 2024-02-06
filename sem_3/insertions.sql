db65130a176a3e6820a76804b21d8158b00ba9ac
Yaseniya Lushko a.k.a. Esya <56122211+Esi4ka@users.noreply.github.com>
2024-02-06 18:29:31 +0300

    Update insertions.sql

--- sem_3/insertions.sql
+++ sem_3/insertions.sql
@@ -9,13 +9,13 @@ salary_amt DECIMAL (12,2),
 salary_type SMALLINT
 );
 
-INSERT INTO sem_3.db_instructor_salary VALUES ('Роздухова Нина', '2019-02-25', 2999.00, 1);
-INSERT INTO sem_3.db_instructor_salary VALUES ('Роздухова Нина', '2019-03-05', 5100.00, 1);
-INSERT INTO sem_3.db_instructor_salary VALUES ('Роздухова Нина', '2019-03-05', 6800.00, 3);
-INSERT INTO sem_3.db_instructor_salary VALUES ('Халяпов Александр', '2019-02-25', 10499.00, 1);
-INSERT INTO sem_3.db_instructor_salary VALUES ('Халяпов Александр', '2019-03-05', 13000.00, 1);
-INSERT INTO sem_3.db_instructor_salary VALUES ('Меркурьева Надежда', '2019-02-25', 2999.00, 1);
-INSERT INTO sem_3.db_instructor_salary VALUES ('Меркурьева Надежда', '2019-02-25', 5800.00, 2);
-INSERT INTO sem_3.db_instructor_salary VALUES ('Меркурьева Надежда', '2019-03-05', 6400.00, 1);
-INSERT INTO sem_3.db_instructor_salary VALUES ('Меркурьева Надежда', '2019-03-05', 8300.00, 2);
+INSERT INTO sem_3.db_instructor_salary VALUES ('Смолкина Юлия', '2019-02-25', 2999.00, 1);
+INSERT INTO sem_3.db_instructor_salary VALUES ('Викентьев Михаил', '2019-03-05', 5100.00, 1);
+INSERT INTO sem_3.db_instructor_salary VALUES ('Викентьев Михаил', '2019-03-05', 6800.00, 3);
+INSERT INTO sem_3.db_instructor_salary VALUES ('Лушко Есения', '2019-02-25', 10499.00, 1);
+INSERT INTO sem_3.db_instructor_salary VALUES ('Лушко Есения', '2019-03-05', 13000.00, 1);
+INSERT INTO sem_3.db_instructor_salary VALUES ('Шевченко Игорь', '2024-02-25', 2999.00, 1);
+INSERT INTO sem_3.db_instructor_salary VALUES ('Шевченко Игорь', '2024-02-25', 5800.00, 2);
+INSERT INTO sem_3.db_instructor_salary VALUES ('Шевченко Игорь', '2024-03-05', 6400.00, 1);
+INSERT INTO sem_3.db_instructor_salary VALUES ('Шевченко Игорь', '2024-03-05', 8300.00, 2);
 
